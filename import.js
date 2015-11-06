f38f3b95dcb7db3a0ebd1148b8e2053d7fbe8278
Julian Simioni <julian@simioni.org>
2015-11-06 15:31:44 -0500

    Move final import stream logic to separate file

--- import.js
+++ import.js
@@ -1,11 +1,7 @@
 var fs = require( 'fs' );
 var glob = require( 'glob' );
-var peliasModel = require('pelias-model');
-var createPeliasElasticsearchPipeline = require('./src/elasticsearchPipeline');
 
 var files = glob.sync( '../../whosonfirst-data/data/**/*[0-9].geojson' );
-
-var Document = require('pelias-model').Document;
 
 console.log('importing ' + files.length + ' files');
 
@@ -40,26 +36,6 @@ files.forEach( function forEach( wofFile ) {
 
 console.log(Object.keys(wofRecords).length + ' records loaded');
 
-var stream_array = require('stream-array');
-var map_stream = require('through2-map');
-var filter_stream = require('through2-filter');
+var importStream = require('./src/importStream');
 
-var id_stream = stream_array(Object.keys(wofRecords));
-
-var object_getter_stream = map_stream.obj(function(id) {
-  return wofRecords[id];
-});
-
-var document_stream = map_stream.obj(function(record) {
-  var wofDoc = new peliasModel.Document( 'whosonfirst', record.id );
-  if (record.name) {
-    wofDoc.setName('default', record.name);
-  }
-  wofDoc.setCentroid({ lat: record.lat, lon: record.lon});
-
-  return wofDoc;
-});
-
-id_stream.pipe(object_getter_stream)
-.pipe(document_stream)
-.pipe(createPeliasElasticsearchPipeline());
+importStream(wofRecords);
