fc367461e6966352448bb2c5a0c6d68af920ff92
Julian Simioni <julian@simioni.org>
2015-11-06 16:24:47 -0500

    Stream in files using fs-extra.walk

--- import.js
+++ import.js
@@ -1,41 +1,54 @@
 var fs = require( 'fs' );
 var glob = require( 'glob' );
+var fs_extra = require('fs-extra');
+var map_stream = require('through2-map');
+var filter_stream = require('through2-filter');
 
-var files = glob.sync( '../../whosonfirst-data/data/**/*[0-9].geojson' );
-
-console.log('importing ' + files.length + ' files');
+var directory = '../../whosonfirst-data/data/';
 
 var wofRecords = {};
 
-files.forEach( function forEach( wofFile ) {
-  var wofRecord = JSON.parse(fs.readFileSync(wofFile));
+var filename_stream = fs_extra.walk(directory);
 
-  var id = wofRecord.id;
+var filter_directory_stream = filter_stream.obj(function(stats_object) {
+  return stats_object.stats.isFile();
+});
 
-  if (!id) {
-    return;
-  }
+var json_parse_stream = map_stream.obj(function(stats_object) {
+  return JSON.parse(fs.readFileSync(stats_object.path));
+});
 
-  if (!wofRecord.hasOwnProperty('properties')) {
-    return;
-  }
+var filter_bad_files_stream = filter_stream.obj(function(json_object) {
+  return json_object.id && json_object.hasOwnProperty('properties');
+});
 
-  if (wofRecords[id]) { // ignore duplicates
-    return;
-  }
-
-  wofRecords[id] = {
-    id: id,
+var object_map_function = function(wofRecord) {
+  return {
+    id: wofRecord.id,
     name: wofRecord.properties['wof:name'],
     // 'h': wofRecord.properties['wof:hierarchy'],
     lat: wofRecord.properties['geom:latitude'],
     lon: wofRecord.properties['geom:longitude'],
     pt: wofRecord.properties['wof:placetype']
   };
+}
+
+var map_fields_stream = map_stream.obj(object_map_function);
+
+var save_object_stream = map_stream.obj(function(obj) {
+  wofRecords[obj.id] = obj;
+  return obj;
 });
 
-console.log(Object.keys(wofRecords).length + ' records loaded');
+filename_stream.pipe(filter_directory_stream)
+.pipe(json_parse_stream)
+.pipe(filter_bad_files_stream)
+.pipe(map_fields_stream)
+.pipe(save_object_stream)
+.on('finish', function() {
+  console.log(Object.keys(wofRecords).length + ' records loaded');
 
-var importStream = require('./src/importStream');
+  var importStream = require('./src/importStream');
 
-importStream(wofRecords);
+  importStream(wofRecords);
+});
