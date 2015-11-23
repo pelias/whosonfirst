6a86b21d1598c51124b321afe903a5dc59ed220e
Stephen Hess <stephen.k.hess@gmail.com>
2015-11-23 14:56:26 -0500

    switched initial data file read to use meta CSV files instead of walking the filesystem tree

--- src/readStream.js
+++ src/readStream.js
@@ -1,22 +1,59 @@
-var fs_extra = require('fs-extra');
+var es = require('event-stream');
+var parse = require('csv-parse');
+var map = require('through2-map');
+var fs = require('fs');
+var spy = require('through2-spy');
+var filter = require('through2-filter');
+var batch = require('batchflow');
+var sink = require('through2-sink');
 
 var readStreamComponents = require('./readStreamComponents');
 
 function readData(directory, wofRecords, callback) {
-  var filename_stream = fs_extra.walk(directory);
+  var types = [
+    'continent',
+    'country',
+    'county',
+    'dependency',
+    'disputed',
+    'empire',
+    'localadmin',
+    'locality',
+    'macrocounty',
+    'macrohood',
+    'macroregion',
+    'metroarea',
+    'microhood',
+    'neighbourhood',
+    'region'
+  ];
 
-  var filter_directory_stream = readStreamComponents.filter_directory_stream();
-  var json_parse_stream = readStreamComponents.json_parse_stream();
-  var filter_bad_files_stream = readStreamComponents.filter_bad_files_stream();
-  var filter_unsupported_placetypes_stream = readStreamComponents.filter_unsupported_placetypes_stream();
-  var map_fields_stream = readStreamComponents.map_fields_stream(wofRecords);
+  batch(types).parallel(2).each(function(idx, type, done) {
+    var csv_parser = parse({ delimiter: ',', columns: true });
+    var is_valid_data_file_path = readStreamComponents.is_valid_data_file_path();
+    var normalize_file_path = readStreamComponents.normalize_file_path();
+    var json_parse_stream = readStreamComponents.json_parse_stream(directory + 'data/');
+    var filter_incomplete_files_stream = readStreamComponents.filter_incomplete_files_stream();
+    var map_fields_stream = readStreamComponents.map_fields_stream();
 
-  filename_stream.pipe(filter_directory_stream)
-  .pipe(json_parse_stream)
-  .pipe(filter_bad_files_stream)
-  .pipe(filter_unsupported_placetypes_stream)
-  .pipe(map_fields_stream)
-  .on('finish', callback);
+    fs.createReadStream(directory + 'meta/wof-' + type + '-latest.csv')
+    .pipe(csv_parser)
+    .pipe(is_valid_data_file_path)
+    .pipe(normalize_file_path)
+    .pipe(json_parse_stream)
+    .pipe(filter_incomplete_files_stream)
+    .pipe(map_fields_stream)
+    .pipe(sink.obj(function(wofRecord) {
+      wofRecords[wofRecord.id] = wofRecord;
+    }))
+    .on('finish', done);
+
+  }).error(function(err) {
+    console.error(err);
+  }).end(function(results) {
+    callback();
+  });
+
 }
 
 module.exports = readData;
--- test/readStreamComponentsTest.js
+++ test/readStreamComponentsTest.js
@@ -21,12 +21,12 @@ function test_stream(input, testedStream, callback) {
 }
 
 tape('readStreamComponents', function(test) {
-  test.test('filter_bad_files_stream filters objects without id and properties', function(t) {
+  test.test('filter_incomplete_files_stream filters objects without id and properties', function(t) {
     var input = [{ id: 5, properties: {}}, { not_id: 6 }];
     var expected = [{id: 5, properties: {} }];
-    var filter_bad_files_stream = readStreamComponents.filter_bad_files_stream();
+    var filter_incomplete_files_stream = readStreamComponents.filter_incomplete_files_stream();
 
-    test_stream(input, filter_bad_files_stream, function(err, actual) {
+    test_stream(input, filter_incomplete_files_stream, function(err, actual) {
       t.deepEqual(actual, expected, 'stream should contain only objects with id and properties');
       t.end();
     });
