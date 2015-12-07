d0a5cb99447d371ac101a966f39d8e2a9328f8d2
Stephen Hess <stephen.k.hess@gmail.com>
2015-12-07 09:25:52 -0500

    extracted test setup/teardown out to helper functions

--- test/readStreamTest.js
+++ test/readStreamTest.js
@@ -10,53 +10,7 @@ tape('readStream', function(test) {
     all that painful.
   */
   test.test('readStream should return from all requested types and populate wofRecords', function(t) {
-    // remove tmp directory if for some reason it's been hanging around from a previous run
-    fs.removeSync('tmp');
-
-    // shell out since creating multiple levels of directories in node is tedious
-    fs.mkdirsSync('tmp/meta');
-    fs.mkdirsSync('tmp/data/1/2/3');
-    fs.mkdirsSync('tmp/data/5/6/7');
-    fs.mkdirsSync('tmp/data/9/10/11');
-
-    // write out first meta and data files
-    fs.writeFileSync('tmp/meta/wof-type1-latest.csv', 'path\n1/2/3/4.geojson\n');
-    fs.writeFileSync('tmp/data/1/2/3/4.geojson', JSON.stringify({
-      id: 4,
-      properties: {
-        'wof:name': 'name 1',
-        'wof:placetype': 'place type 1',
-        'wof:parent_id': 2,
-        'geom:latitude': 12.121212,
-        'geom:longitude': 21.212121
-      }
-    }));
-
-    // write out second meta and data files
-    fs.writeFileSync('tmp/meta/wof-type2-latest.csv', 'path\n5/6/7/8.geojson\n');
-    fs.writeFileSync('tmp/data/5/6/7/8.geojson', JSON.stringify({
-      id: 8,
-      properties: {
-        'wof:name': 'name 2',
-        'wof:placetype': 'place type 2',
-        'wof:parent_id': 3,
-        'geom:latitude': 13.131313,
-        'geom:longitude': 31.313131
-      }
-    }));
-
-    // write out third meta and data files that are ignored
-    fs.writeFileSync('tmp/meta/wof-type3-latest.csv', 'path\n9/10/11/12.geojson\n');
-    fs.writeFileSync('tmp/data/9/10/11/12.geojson', JSON.stringify({
-      id: 12,
-      properties: {
-        'wof:name': 'name 3',
-        'wof:placetype': 'place type 3',
-        'wof:parent_id': 4,
-        'geom:latitude': 14.141414,
-        'geom:longitude': 41.414141
-      }
-    }));
+    setupTestEnvironment();
 
     var wofRecords = {};
 
@@ -81,13 +35,69 @@ tape('readStream', function(test) {
         lon: 31.313131
       }, 'id 8 should have been loaded');
 
-      // cleanup
-      fs.removeSync('tmp');
-
       t.end();
+
+      cleanupTestEnvironment();
 
     });
 
   });
 
 });
+
+function setupTestEnvironment() {
+  // remove tmp directory if for some reason it's been hanging around from a previous run
+  fs.removeSync('tmp');
+
+  fs.mkdirsSync('tmp/meta');
+
+  // write out first meta and data files
+  fs.writeFileSync('tmp/meta/wof-type1-latest.csv', 'path\n1/2/3/4.geojson\n');
+  fs.mkdirsSync('tmp/data/1/2/3');
+  fs.writeFileSync('tmp/data/1/2/3/4.geojson', JSON.stringify({
+    id: 4,
+    properties: {
+      'wof:name': 'name 1',
+      'wof:placetype': 'place type 1',
+      'wof:parent_id': 2,
+      'geom:latitude': 12.121212,
+      'geom:longitude': 21.212121
+    }
+  }));
+
+  // write out second meta and data files
+  fs.writeFileSync('tmp/meta/wof-type2-latest.csv', 'path\n5/6/7/8.geojson\n');
+  fs.mkdirsSync('tmp/data/5/6/7');
+  fs.writeFileSync('tmp/data/5/6/7/8.geojson', JSON.stringify({
+    id: 8,
+    properties: {
+      'wof:name': 'name 2',
+      'wof:placetype': 'place type 2',
+      'wof:parent_id': 3,
+      'geom:latitude': 13.131313,
+      'geom:longitude': 31.313131
+    }
+  }));
+
+  // write out third meta and data files that are ignored
+  // it will be ignored since 'type3' is not passed as a supported type
+  // this shows that types are supported instead of all files being globbed
+  fs.writeFileSync('tmp/meta/wof-type3-latest.csv', 'path\n9/10/11/12.geojson\n');
+  fs.mkdirsSync('tmp/data/9/10/11');
+  fs.writeFileSync('tmp/data/9/10/11/12.geojson', JSON.stringify({
+    id: 12,
+    properties: {
+      'wof:name': 'name 3',
+      'wof:placetype': 'place type 3',
+      'wof:parent_id': 4,
+      'geom:latitude': 14.141414,
+      'geom:longitude': 41.414141
+    }
+  }));
+
+}
+
+function cleanupTestEnvironment() {
+  fs.removeSync('tmp');
+
+}
