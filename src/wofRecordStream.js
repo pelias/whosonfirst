5bca8e7210bccd1a2428d068fb551f5edea2da5f
Stephen Hess <stephen.k.hess@gmail.com>
2015-12-11 16:03:18 -0500

    refactored to be less code

--- src/wofRecordStream.js
+++ src/wofRecordStream.js
@@ -1,5 +1,4 @@
 var event_stream = require('event-stream');
-var _ = require('lodash');
 
 module.exports = {};
 
@@ -7,13 +6,11 @@ module.exports.createWofRecordsStream = function(wofRecords) {
   var ids = Object.keys(wofRecords);
 
   return event_stream.readable(function (count, callback) {
-    var id = ids.shift();
-
-    if (_.isUndefined(id)) {
+    if (ids.length === 0) {
       return this.emit('end');
     }
 
-    this.emit('data', wofRecords[id]);
+    this.emit('data', wofRecords[ids.shift()]);
     callback();
 
   });
