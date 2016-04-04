f72bdd95e08eafbc8588967cbf83a5f778c63e05
Stephen Hess <stephen.k.hess@gmail.com>
2016-04-04 13:56:46 -0400

    de-anonymized single exported function

--- src/components/isValidId.js
+++ src/components/isValidId.js
@@ -1,6 +1,6 @@
 var filter = require('through2-filter');
 
-module.exports.create = function() {
+module.exports.create = function isValidId() {
   return filter.obj(function(record) {
     return record.hasOwnProperty('id') &&
             record.id &&
