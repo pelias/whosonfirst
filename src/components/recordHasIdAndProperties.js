ccb8658ed49dc9dab2d48749f40a90d94d65bd03
Stephen Hess <stephen.k.hess@gmail.com>
2016-04-04 14:00:15 -0400

    de-anonymized single exported function

--- src/components/recordHasIdAndProperties.js
+++ src/components/recordHasIdAndProperties.js
@@ -3,7 +3,7 @@ var filter = require('through2-filter');
 /*
   This function filters out incomplete records
 */
-module.exports.create = function() {
+module.exports.create = function create() {
   return filter.obj(function(json_object) {
     return json_object.id && json_object.hasOwnProperty('properties');
   });
