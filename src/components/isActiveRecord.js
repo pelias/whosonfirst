35e558b69609c0967364ca6fd575caef375ac4e8
Stephen Hess <stephen.k.hess@gmail.com>
2016-04-04 13:58:53 -0400

    de-anonymized single exported function

--- src/components/isActiveRecord.js
+++ src/components/isActiveRecord.js
@@ -1,7 +1,7 @@
 var filter = require('through2-filter');
 var _ = require('lodash');
 
-module.exports.create = function() {
+module.exports.create = function create() {
   return filter.obj(function(wofData) {
     return _.isEmpty(_.trim(wofData.properties['edtf:deprecated']));
   });
