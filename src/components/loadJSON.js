0896d400a1d65efbecbe6b0921c75cc88023a692
Julian Simioni <julian@mapzen.com>
2016-12-06 11:09:52 -0500

    Improve JSON parsing error message

--- src/components/loadJSON.js
+++ src/components/loadJSON.js
@@ -14,7 +14,7 @@ module.exports.create = function create_json_parse_stream(dataDirectory) {
           var object = JSON.parse(data);
           next(null, object);
         } catch (parse_err) {
-          console.error('exception on %s:', record.path, parse_err);
+          console.error('exception parsing JSON in file %s:', record.path, parse_err);
           console.error('Inability to parse JSON usually means that WOF has been cloned ' +
                         'without using git-lfs, please see instructions here: ' +
                         'https://github.com/whosonfirst/whosonfirst-data#git-and-large-files');
