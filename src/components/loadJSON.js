4f59de4212fdfd45034b63c5b4bc6898f993e4eb
Stephen Hess <stephen.k.hess@gmail.com>
2017-05-03 12:45:16 -0400

    added support for fatal and non-fatal missing file conditions
    added tests and refactored code a bit for clarity
    log to pelias-logger instead of stderr

--- src/components/loadJSON.js
+++ src/components/loadJSON.js
@@ -1,27 +1,37 @@
-var fs = require('fs');
-var parallelStream = require('pelias-parallel-stream');
+const path = require('path');
+const fs = require('fs');
+const parallelStream = require('pelias-parallel-stream');
 
-var maxInFlight = 10;
+const maxInFlight = 10;
 
-module.exports.create = function create_json_parse_stream(dataDirectory) {
+const logger = require( 'pelias-logger' ).get( 'whosonfirst' );
+
+module.exports.create = function create(wofRoot, missingFilesAreFatal) {
   return parallelStream(maxInFlight, function(record, enc, next) {
-    var full_file_path = dataDirectory + record.path;
-    fs.readFile(full_file_path, function(err, data) {
+    const full_file_path = [ wofRoot, 'data', record.path ].join(path.sep);
+
+    fs.readFile(full_file_path, (err, data) => {
       if (err) {
-        console.error('exception reading file ' + full_file_path);
-        next(err);
-      } else {
-        try {
-          var object = JSON.parse(data);
-          next(null, object);
-        } catch (parse_err) {
-          console.error('exception parsing JSON in file %s:', record.path, parse_err);
-          console.error('Inability to parse JSON usually means that WOF has been cloned ' +
-                        'without using git-lfs, please see instructions here: ' +
-                        'https://github.com/whosonfirst/whosonfirst-data#git-and-large-files');
-          next(parse_err);
-        }
+        logger.error(err.message);
+
+        // only forward the error is missing files should be fatal
+        return next(missingFilesAreFatal ? err : null);
+
       }
+
+      try {
+        next(null, JSON.parse(data));
+
+      } catch (parse_err) {
+        logger.error(`exception parsing JSON in file ${record.path}: ${parse_err}`);
+        logger.error('Inability to parse JSON usually means that Who\'s on First ' +
+                      'has been cloned without using git-lfs, please see instructions ' +
+                      'here: https://github.com/whosonfirst/whosonfirst-data#git-and-large-files');
+        next(parse_err);
+        
+      }
+
     });
+
   });
 };
--- test/components/loadJSONTest.js
+++ test/components/loadJSONTest.js
@@ -1,101 +1,133 @@
-var tape = require('tape');
-var event_stream = require('event-stream');
-var intercept = require('intercept-stdout');
-var fs = require('fs');
-
-var loadJSON = require('../../src/components/loadJSON');
+const tape = require('tape');
+const event_stream = require('event-stream');
+const path = require('path');
+const fs = require('fs');
+const temp = require('temp').track();
+const proxyquire = require('proxyquire').noCallThru();
 
 function test_stream(input, testedStream, callback, error_callback) {
-    if (!error_callback) {
-      error_callback = function() {};
-    }
-
     if (!callback) {
       callback = function() {};
     }
 
-    var input_stream = event_stream.readArray(input);
-    var destination_stream = event_stream.writeArray(callback);
+    const input_stream = event_stream.readArray(input);
+    const destination_stream = event_stream.writeArray(callback);
 
-    input_stream.pipe(testedStream).on('error', error_callback)
-    .pipe(destination_stream);
+    input_stream
+      .pipe(testedStream)
+      .on('error', error_callback || () => {})
+      .pipe(destination_stream);
+
 }
 
-tape('loadJSON', function(test) {
-  test.test('json_parse_stream should throw error if the file is not readable', function(t) {
-    var input = [
-      {
-        path: 'this_file_does_not_exist.json'
-      }
-    ];
+tape('loadJSON tests', (test) => {
+  test.test('json should be loaded from file', (t) => {
+    temp.mkdir('tmp_wof_data', (err, temp_dir) => {
+      fs.mkdirSync([temp_dir, 'data'].join(path.sep));
 
-    var stderr = '';
+      // write the contents to a file
+      const filename = [temp_dir, 'data', 'datafile.geojson'].join(path.sep);
+      fs.writeFileSync(filename, '{ "a": 1, "b": 2 }\n');
 
-    // intercept/swallow stderr
-    var unhook_intercept = intercept(
-      function() { },
-      function(txt) { stderr += txt; return ''; }
-    );
+      const loadJSON = require('../../src/components/loadJSON');
 
-    test_stream(input, loadJSON.create('./'), undefined, function(err, actual) {
-      t.deepEqual(actual, undefined, 'an error should be thrown');
-      t.ok(stderr.match(/exception reading file/), 'error output present');
-      console.log(stderr);
-      unhook_intercept();
-      t.end();
-    });
-  });
+      const input = {
+        path: path.basename(filename)
+      };
 
-  test.test('json_parse_stream should throw error if the file is not json', function(t) {
-    var input = [
-      {
-        path: 'this_is_not_json.json'
-      }
-    ];
+      test_stream([input], loadJSON.create(temp_dir), (err, actual) => {
+        temp.cleanupSync();
+        t.deepEqual(actual, [{ a: 1, b: 2 }], 'should be equal');
+        t.end();
+      });
 
-    var stderr = '';
-
-    // intercept/swallow stderr
-    var unhook_intercept = intercept(
-      function() { },
-      function(txt) { stderr += txt; return ''; }
-    );
-
-    fs.writeFileSync(input[0].path, 'this is not JSON');
-
-    test_stream(input, loadJSON.create('./'), undefined, function(err, actual) {
-      t.deepEqual(actual, undefined, 'an error should be thrown');
-      t.ok(stderr.match(/SyntaxError: Unexpected token h/), 'error output present');
-      unhook_intercept();
-      fs.unlinkSync(input[0].path);
-      t.end();
     });
 
   });
 
-  test.test('json_parse_stream should parse filename and return parsed object', function(t) {
-    var input = [
-      {
-        path: 'parse_stream_test.json'
-      }
-    ];
+  test.test('invalid JSON should log an error and not pass along anything', (t) => {
+    const logger = require('pelias-mock-logger')();
 
-    var expected = [{
-      field1: 'value1',
-      field2: 'value2',
-      nested: {
-        field3: 'value3',
-        field4: 'value4'
-      },
-      array: ['value5', 'value6']
-    }];
+    temp.mkdir('tmp_wof_data', (err, temp_dir) => {
+      fs.mkdirSync([temp_dir, 'data'].join(path.sep));
 
-    fs.writeFileSync(input[0].path, JSON.stringify(expected[0]) + '\n');
+      const loadJSON = proxyquire('../../src/components/loadJSON', {
+        'pelias-logger': logger
+      });
 
-    test_stream(input, loadJSON.create('./'), function(err, actual) {
-      t.deepEqual(actual, expected, 'stream should contain only objects with id and properties');
-      t.end();
-      fs.unlinkSync(input[0].path);
+      // write the contents to a file
+      const filename = [temp_dir, 'data', 'datafile.geojson'].join(path.sep);
+      fs.writeFileSync(filename, 'this is not json\n');
+
+      const input = {
+        path: path.basename(filename)
+      };
+
+      test_stream([input], loadJSON.create(temp_dir), undefined, (err, actual) => {
+        temp.cleanupSync();
+        t.deepEqual(actual, undefined, 'an error should be thrown');
+        t.ok(logger.isErrorMessage(/SyntaxError: Unexpected token h/), 'error output present');
+        t.end();
+      });
+
+    });
+
+  });
+
+  test.test('missing file should be non-fatal by default', (t) => {
+    const logger = require('pelias-mock-logger')();
+
+    temp.mkdir('tmp_wof_data', (err, temp_dir) => {
+      fs.mkdirSync([temp_dir, 'data'].join(path.sep));
+
+      const loadJSON = proxyquire('../../src/components/loadJSON', {
+        'pelias-logger': logger
+      });
+
+      // non-existent file
+      const input = {
+        path: path.basename([temp_dir, 'data', 'datafile.geojson'].join(path.sep))
+      };
+
+      test_stream([input], loadJSON.create(temp_dir), (err, actual) => {
+        temp.cleanupSync();
+        t.ok(logger.isErrorMessage(/ENOENT: no such file or directory/), 'error output present');
+        t.end();
+      }, (err) => {
+        // because loadJSON uses parallelStream internally, the only way to test
+        // that the error wasn't passed to next() is to handle in the error callback
+        t.fail('error callback should not have been called since missing files are non-fatal');
+      });
+
+    });
+
+  });
+
+  test.test('missing file should be fatal when specified by parameter', (t) => {
+    const logger = require('pelias-mock-logger')();
+
+    temp.mkdir('tmp_wof_data', (err, temp_dir) => {
+      fs.mkdirSync([temp_dir, 'data'].join(path.sep));
+
+      const loadJSON = proxyquire('../../src/components/loadJSON', {
+        'pelias-logger': logger
+      });
+
+      // non-existent file
+      const input = {
+        path: path.basename([temp_dir, 'data', 'datafile.geojson'].join(path.sep))
+      };
+
+      test_stream([input], loadJSON.create(temp_dir, true), (err, actual) => {
+        t.ok(logger.isErrorMessage(/ENOENT: no such file or directory/), 'error output present');
+        temp.cleanupSync();
+        t.end();
+      }, (err) => {
+        // because loadJSON uses parallelStream internally, the only way to test
+        // that the error was passed to next() is to handle in the error callback
+        t.ok(err);
+      });
+
     });
 
   });
