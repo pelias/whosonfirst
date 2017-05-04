2e4516dc7382e9d76e7219cee019dea6a6d83954
Stephen Hess <stephen.k.hess@gmail.com>
2017-05-03 21:14:57 -0400

    added helper function

--- src/components/recordNotVisitingNullIsland.js
+++ src/components/recordNotVisitingNullIsland.js
@@ -6,11 +6,15 @@ var _ = require('lodash');
 
   Filter out any postalcodes located at 0,0 because there are currently too many of those
 */
+
+function isNullIslandPostalCode(record) {
+  return record.placetype === 'postalcode' &&
+          record.geom_latitude === '0.0' &&
+          record.geom_longitude === '0.0';
+}
+
 module.exports.create = function create() {
   return filter.obj((record) => {
-    if (record.placetype === 'postalcode' && record.geom_latitude === '0.0' && record.geom_longitude === '0.0') {
-      return false;
-    }
-    return true;
+    return !isNullIslandPostalCode(record);
   });
 };
