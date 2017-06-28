468b478dc1fe5f6bf082bf6e8ddf5936278eea99
Diana Shkolnikov <dianashk@gmail.com>
2017-06-28 00:22:56 -0400

    split download for all and filtered

--- utils/download_data.js
+++ utils/download_data.js
@@ -1,3 +1,12 @@
-/**
- * Created by diana on 6/27/17.
- */
+const config = require( 'pelias-config' ).generate(require('../schema'));
+
+function on_done() {
+  console.log('All done!');
+}
+
+if (config.imports.whosonfirst.importPlace) {
+  require('./download_data_filtered/index').download(on_done);
+}
+else {
+  require('./download_data_all').download(on_done);
+}
