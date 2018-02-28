b0f7de07a8082a2bf10836a4be75d6496fbc11a7
missinglink <insomnia@rcpt.at>
2018-02-28 20:47:48 +0100

    update from mapzen.com urls -> whosonfirst.org

--- src/bundleList.js
+++ src/bundleList.js
@@ -62,7 +62,7 @@ function getPlacetypes() {
 
 function ensureBundleIndexExists(metaDataPath) {
   const bundleIndexFile = path.join(metaDataPath, 'whosonfirst_bundle_index.txt');
-  const bundleIndexUrl = 'https://whosonfirst.mapzen.com/bundles/index.txt';
+  const bundleIndexUrl = 'https://dist.whosonfirst.org/bundles/index.txt';
 
   //ensure required directory structure exists
   fs.ensureDirSync(metaDataPath);
