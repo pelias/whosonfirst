b8c395e348dfdc2e0a400acf95c9faa324798921
Richard Calahan <richard@calahan.me>
2018-09-04 10:10:10 -0700

    updated additional bundle filename

--- src/components/metadataStream.js
+++ src/components/metadataStream.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = (wofRoot) => {
   return {
     create: (placetype) => {
-      return fs.createReadStream(path.join(wofRoot, 'meta', `wof-${placetype}-latest.csv`));
+      return fs.createReadStream(path.join(wofRoot, 'meta', `whosonfirst-data-${placetype}-latest.csv`));
     }
   };
 };
