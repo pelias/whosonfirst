7b7d58af9eedb9ea6eb67f5a5a27a100c11a22bc
Joxit <joxit972@gmail.com>
2019-01-11 23:18:05 +0100

    feat(sqlite): Add test for SQLite read stream
    Stick combined-stream to v1.0.5 due to https://github.com/felixge/node-combined-stream/issues/40

--- src/components/sqliteStream.js
+++ src/components/sqliteStream.js
@@ -3,14 +3,14 @@ const Sqlite3 = require('better-sqlite3');
 
 class SQLiteStream extends Readable {
   constructor(dbPath, sql) {
-    super({ encoding: 'utf-8', autoDestroy: true });
+    super({ objectMode: true, autoDestroy: true });
     this._db = new Sqlite3(dbPath, { readonly: true });
     this._iterator = this._db.prepare(sql).iterate();
   }
 
   _read() {
     const elt = this._iterator.next();
-    if(!this.push(!elt.done && elt.value ? elt.value.body : null)) {
+    if(!this.push(!elt.done && elt.value ? elt.value : null)) {
       this._db.close();
     }
   }
