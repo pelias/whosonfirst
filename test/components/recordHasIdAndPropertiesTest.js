9be5fe0afbea676821aa269f4db86ef8b9abb89a
Michael Kirk <michael.code@endoftheworl.de>
2023-12-12 02:24:46 -0800

    chore(dependencies): replace event-stream with stream-mock (#543)
    Part of https://github.com/pelias/pelias/issues/801
    Technique cribbed from https://github.com/pelias/openaddresses/pull/457

--- test/components/recordHasIdAndPropertiesTest.js
+++ test/components/recordHasIdAndPropertiesTest.js
@@ -1,13 +1,14 @@
 var tape = require('tape');
-var event_stream = require('event-stream');
+const stream_mock = require('stream-mock');
 
 var recordHasIdAndProperties = require('../../src/components/recordHasIdAndProperties');
 
 function test_stream(input, testedStream, callback) {
-    var input_stream = event_stream.readArray(input);
-    var destination_stream = event_stream.writeArray(callback);
-
-    input_stream.pipe(testedStream).pipe(destination_stream);
+  const reader = new stream_mock.ObjectReadableMock(input);
+  const writer = new stream_mock.ObjectWritableMock();
+  writer.on('error', (e) => callback(e));
+  writer.on('finish', () => callback(null, writer.data));
+  reader.pipe(testedStream).pipe(writer);
 }
 
 tape('recordHasIdAndProperties', function(test) {
--- test/components/recordHasNameTest.js
+++ test/components/recordHasNameTest.js
@@ -1,13 +1,14 @@
 var tape = require('tape');
-var event_stream = require('event-stream');
+var stream_mock = require('stream-mock');
 
 var recordHasName = require('../../src/components/recordHasName');
 
 function test_stream(input, testedStream, callback) {
-    var input_stream = event_stream.readArray(input);
-    var destination_stream = event_stream.writeArray(callback);
-
-    input_stream.pipe(testedStream).pipe(destination_stream);
+  const reader = new stream_mock.ObjectReadableMock(input);
+  const writer = new stream_mock.ObjectWritableMock();
+  writer.on('error', (e) => callback(e));
+  writer.on('finish', () => callback(null, writer.data));
+  reader.pipe(testedStream).pipe(writer);
 }
 
 tape('recordHasName', function(test) {
