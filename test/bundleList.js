9646530fd35c643c644af84fb797839c10b91277
arnesetzer <25772747+arnesetzer@users.noreply.github.com>
2025-09-01 13:47:38 +0200

    test: Check whether files in wof root and sqlite directory work as well

--- test/bundleList.js
+++ test/bundleList.js
@@ -65,7 +65,7 @@ tape('bundlesList tests', (test) => {
         temp.cleanup((err) => {
           t.notOk(err);
           t.notOk(e);
-          t.deepEqual(bundlesList, SQLITE_EXAMPLE);
+          t.deepEqual(bundlesList.sort(), SQLITE_EXAMPLE);
           t.end();
         });
       });
@@ -124,6 +124,45 @@ tape('bundlesList tests', (test) => {
           t.notOk(err);
           t.notOk(e);
           t.deepEqual(bundlesList, SQLITE_EXAMPLE);
+          t.end();
+        });
+      });
+    });
+  });
+  test.test('sqlite in root dir and sqlite folder', (t) => {
+    temp.mkdir('supports_sqlite', (err, temp_dir) => {
+      fs.mkdirSync(temp_dir, { recursive: true });
+      fs.mkdirSync(path.join(temp_dir, 'sqlite'), { recursive: true });
+      const config = {
+        generate: () => {
+          return peliasConfig.generateCustom({
+            imports: {
+              whosonfirst: {
+                datapath: temp_dir,
+                sqlite: true
+              }
+            }
+          });
+        }
+      };
+      const halfLength = Math.ceil(SQLITE_EXAMPLE.length / 2);
+      SQLITE_EXAMPLE
+        .concat(['ignore_me.csv', 'README.md']).slice(0, halfLength)
+        .forEach(e => fs.writeFileSync(path.join(temp_dir, e), ''));
+        
+      SQLITE_EXAMPLE
+        .concat(['ignore_me.csv', 'README.md'])
+        .slice(halfLength)
+        .forEach(e => fs.writeFileSync(path.join(temp_dir,'sqlite', e), ''));
+
+      const bundles = proxyquire('../src/bundleList', { 'pelias-config': config });
+
+      bundles.generateBundleList((e, bundlesList) => {
+        temp.cleanup((err) => {
+          t.notOk(err);
+          t.notOk(e);
+          //Due to the split above we need to sort before comparison. Otherwise order is wrong and the test fails
+          t.deepEqual(bundlesList.sort(), SQLITE_EXAMPLE);
           t.end();
         });
       });
